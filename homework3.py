cf60131eb3c8780ec2bf42b1bb93cb8a7dd3ca3e
Daria <darya.piankova@yandex.ru>
2023-04-16 21:16:10 +0500

    task18

--- homework3.py
+++ homework3.py
@@ -2,5 +2,17 @@ n = int(input('Введите кол-во элементов в массиве: 
 lst = []
 for i in range(n):
     number = lst.append(int(input(f"Введите {i+1}-е число в массив: ")))
-x = int(input('Введите число, кол-во которого надо посчитать в массиве: '))
-print(lst.count(x))
+
+x = int(input('Введите число X: '))
+while(lst.count(x)!=0):
+    x = int(input('Неверно! Число не должно содержать в массиве! Введите число X: '))
+smallestDiff=abs(lst[0]-x)
+nearestElement=lst[0]
+for j in lst:
+    if abs(j-x)<smallestDiff:
+        smallestDiff=abs(j-x)
+        nearestElement=j
+print(nearestElement)
+
+    
+
